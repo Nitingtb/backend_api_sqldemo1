d7db6a1333189f42fed26daa3d4b04c1b7dd11a4
NITIN SIR <nitingtb8@gmail.com>
2025-01-22 16:42:13 +0530

    v3 all reports created in order_report api

--- v3_api_routes.js
+++ v3_api_routes.js
@@ -154,7 +154,136 @@ v3_api_router.get("/order_report",async(req,res)=> {
             $addFields : {month : {$month: "$order_at"}, 
             
             full_month: { $dateToString: { format: "%B", date: "$order_at", timezone: "+04:30" } } }
+        },
+        {
+            $lookup: {
+                from: "orderdetails",
+                localField: "orderNumber",
+                foreignField:"orderNumber",
+                as: "orderDetails",
+                pipeline: [
+                    {
+                        $project : {_id:0 ,amount:{$multiply: ["$priceEach","$quantityOrdered"]}}
+                    },
+                    {
+                        $group: {_id:"orderNumber",total_amount:{$sum:"$amount"}}
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: "$orderDetails"
+        },
+        {
+            $addFields : {total_amount: "$orderDetails.total_amount"}
+        },
+        {
+            $unset : "orderDetails"
+        },
+        {
+            $lookup : {
+                from:"customers",
+                localField: "customerNumber",
+                foreignField: "customerNumber",
+                as : "customers",
+                pipeline : [
+                    {
+                    $project: {_id:0,country:1,salesRepEmployeeNumber:1}
+                    },
+                    {
+                        $lookup: {
+                            from:"employees",
+                            localField: "salesRepEmployeeNumber",
+                            foreignField: "employeeNumber",
+                            as : "emp",
+                            pipeline : [
+                                {
+                                    $project : {lastName:1,officeCode:1,_id:0}
+                                },
+                                {
+                                    $lookup: {
+                                        from : "offices",
+                                        localField: "officeCode",
+                                        foreignField: "officeCode",
+                                        as : "office",
+                                        pipeline: [
+                                            {
+                                                $project: {city:1,_id:0}
+                                            }
+                                        ]
+                                    }
+                                },
+                                {
+                                    $unwind : "$office"
+                                }
+
+                            ]
+                        }
+                    },
+                    {
+                        $unwind : "$emp"
+                    }
+                ]
+            }
+        },
+        {
+            $unwind : "$customers"
+        },
+        {
+            $addFields : {branch: "$customers.emp.office.city"}
+        },
+        {
+            $unset : "customers"
+        },
+        {
+            $group: {_id: {branch:"$branch",month:"$month",monthName:"$full_month"}, total_amount:{$sum:"$total_amount"}}
+        },
+        {
+            $sort: {"_id.branch":1,"_id.month":1}
+        },
+        {
+            $addFields: {branch:"$_id.branch",month:"$_id.monthName"}
+        },
+        {
+            $unset: "_id"
         }
+
+        
+        
+        
+        /*
+        {
+            $addFields: {lastName: "$customers.emp.lastName"}
+        },
+        {
+            $unset : "customers"
+        },
+        {
+            $group: {_id:{lastName:"$lastName", month:"$full_month"}, total_amount:{$sum:"$total_amount"} }
+        },
+        {
+            $sort : { "_id.lastName" : 1, "_id.month":1 }
+        }, 
+        
+            for emp wise report */
+
+
+
+
+        
+        /*
+        {
+            $addFields : { country: "$customers.country" } 
+        },
+        {
+            $group : {_id: {country:"$customers.country",month:"$full_month"} , total_amount:{$sum:"$total_amount"} }
+        },
+        {
+            $sort : { "_id.country" : 1, "_id.month":1 }
+        }, 
+        for country wise reprt */ 
+        
+        
      
     ]);
 
